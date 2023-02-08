780f1fc01102750ba47ef8ff63dca859163a6d64
Kon-Tact <mathiasorlando95@gmail.com>
2023-02-08 13:25:13 +0100

    Fixed

--- app/src/main/java/com/example/test_firebase/RealTimeActivity.java
+++ app/src/main/java/com/example/test_firebase/RealTimeActivity.java
@@ -1,25 +1,36 @@
 package com.example.test_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RealTimeActivity extends AppCompatActivity {
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    FirebaseDatabase database = FirebaseDatabase.getInstance(
+            "https://test-e782f-default-rtdb.europe-west1.firebasedatabase.app"
+            );
+    DatabaseReference messageRef = database.getReference("message");
     private Button btn_Message;
+    private TextView tv_readMessage, tvTestInfo;
     private EditText et_Message;
+    private static final String TAG = "RealTimeActivity";
 
     private void initUi() {
         btn_Message = findViewById(R.id.btn_send);
         et_Message = findViewById(R.id.et_message);
+        tv_readMessage = findViewById(R.id.tv_readMessage);
+        tvTestInfo = findViewById(R.id.tvTestInfo);
     }
 
     @Override
@@ -29,17 +40,45 @@ public class RealTimeActivity extends AppCompatActivity {
 
         initUi();
         sendMessage();
-
-
+        readMessage();
+        readTest();
     }
 
     public void sendMessage() {
-        btn_Message.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myRef.setValue(et_Message.getText().toString());
-            }
-        });
+        btn_Message.setOnClickListener(v -> messageRef.setValue(et_Message.getText().toString()));
 
     }
+
+    public void readMessage() {
+        messageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String message = snapshot.getValue(String.class);
+                tv_readMessage.setText(message);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Echec de la lecture du message ", error.toException());
+            }
+        });
+    }
+
+        public void readTest() {
+            database.getReference("test").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String test = snapshot.getValue(String.class);
+                    tvTestInfo.setText(test);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.w(TAG, "Echec de la lecture du test ", error.toException());
+                }
+            });
+        }
+
+
 }
